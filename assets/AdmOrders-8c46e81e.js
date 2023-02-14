6df6f65bb43f65f3ae899dc27a9d3a363f217d7e
JarDar <jardar.tsai@gmail.com>
2023-02-14 09:15:54 +0800

    deploy

---
+++ assets/AdmOrders-8c46e81e.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as r}from"./index-1d1635aa.js";const n={};function o(t,s){return c(),r("h1",null,"這是後台訂單頁")}const a=e(n,[["render",o]]);export{a as default};
--- assets/AdmOrders-980d92a5.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as r}from"./index-4b7bad84.js";const n={};function o(t,s){return c(),r("h1",null,"這是後台訂單頁")}const a=e(n,[["render",o]]);export{a as default};
